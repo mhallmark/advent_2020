df60aa2cdac6647b9cc834cc7d929c940e172609
Matthew Hallmark <matt@coopdigity.com>
2020-12-02 17:37:23 -0800

    dwa

--- scripts/day1.fsx
+++ scripts/day1.fsx
@@ -1,6 +1,5 @@
 open System.IO
 
-
 let key = 2020
 
 let input =
@@ -13,6 +12,7 @@ let otherAddendExists (sum: int) (add: int) =
     let want = sum - add
     Set.contains want inputSet
 
+// O(n log n) ?
 let found1 =
     input |> Array.find (otherAddendExists key)
 
@@ -22,6 +22,7 @@ printfn "First Answer: %i" answer1
 
 let mul (i, j, k) = i * j * k
 
+// O(n) sq?
 let answer2 =
     input
     |> Array.map (fun i ->
@@ -31,10 +32,14 @@ let answer2 =
             input |> Array.tryFind (otherAddendExists tkey)
 
         match other with
-        | Some o -> Some(i, tkey, o)
+        | Some o ->
+            let t = key - (i + o)
+            Some(i, t, o)
         | None -> None)
-    |> Array.find Option.isSome
-    |> Option.get
-    |> mul
+    |> Array.filter Option.isSome
+    |> Array.map Option.get
+    |> Seq.map mul
+    |> Seq.distinct
+    |> Seq.exactlyOne
 
 printfn "Second Answer: %i" answer2
